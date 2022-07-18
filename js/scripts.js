33f9d3938de9e1a5eb0ada7939c648e1e8ee3629
modskinbr <lucassena021@gmail.com>
2022-07-18 00:56:06 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+window.addEventListener('load', function() {
+	var vs = "6.7.8";
+	var versionid = document.getElementById("version");
+	versionid.innerHTML = vs;
+    console.log( vs + versionid )
+})
