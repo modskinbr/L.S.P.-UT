03b87ad6cbae09f9c0d39d81e505b6ed65f890fb
modskinbr <lucassena021@gmail.com>
2022-09-08 19:33:11 -0300

    yp

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,15 @@
 // Use this file to add JavaScript to your project
 
 window.addEventListener('load', function() {
-	var vs = "6.9.0";
+	var vs = "7.0.0";
+	var dat1 = "2022-09-08T20:00:00-03:00";
+	var dat2 = "2022-09-08T16:30:00-03:00";
 	var versionid = document.getElementById("version");
+	var complet = document.getElementById("d1"); 
+	var fast = document.getElementById("d2");
+	
 	versionid.innerHTML = vs;
+	fast.title = dat2;
+	complet.title = dat1;
     console.log( vs + versionid )
 })
