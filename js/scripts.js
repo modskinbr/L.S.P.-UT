8a4eb0c4f8e5c01a0103b2515733431553d9aecc
modskinbr <lucassena021@gmail.com>
2022-09-25 18:02:43 -0300

    2

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
 window.addEventListener('load', function() {
-	var vs = "7.0.0";
+	var vs = "7.1.0";
 	var versionid = document.getElementById("version");
 	
 	versionid.innerHTML = vs;
