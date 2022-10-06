8ee728e0dea544038cb5e4b87e2d0cc549ffcd30
modskinbr <lucassena021@gmail.com>
2022-10-06 18:12:00 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', function() {
-	var vs = "7.1.0";
+	var vs = "7.2.0";
 	var versionid = document.getElementById("version");
-	
 	versionid.innerHTML = vs;
-    console.log( vs + versionid + fast + dat2 )
 
 })
