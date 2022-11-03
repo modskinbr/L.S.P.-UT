d2e2ac417bc727398434dc0d84e5875f70698e50
modskinbr <lucassena021@gmail.com>
2022-11-02 23:25:19 -0300

    0

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
 window.addEventListener('load', function() {
 	var vs = "7.5.0";
+	var estxt = "⌛";
+	var notext = "🚫";
 	var versionid = document.getElementById("version");
+	var btntext01 = document.getElementById("btn-text01");
+	var btntext02 = document.getElementById("btn-text02");
 	versionid.innerHTML = vs;
-
+	
+	var AreInUpdate = true;
+	var NoUnavailable = true;
+	
+	if ( AreInUpdate === true ) {
+		btntext01.innerText = estxt;
+		btntext01.classList.remove("btn-primary")
+		btntext01.classList.add("btn-dark")
+		btntext01.classList.add("blocked")
+    }
+    if ( NoUnavailable === true ) {
+    	btntext02.innerText = notext;
+		btntext02.classList.remove("btn-primary")
+		btntext02.classList.add("btn-dark")
+		btntext02.classList.add("blocked")
+		}
 })
