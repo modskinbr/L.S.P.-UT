f6bc78fd7a2cb79e97d2831942c47f6822925635
modskinbr <lucassena021@gmail.com>
2022-12-14 22:35:45 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,35 @@
 window.addEventListener('load', function() {
-	var vs = "7.7.0";
-	var estxt = "⌛";
-	var notext = "🚫";
+	var in = "7.7.0";
+	var on = "⌛";
+	var off = "🚫";
 	var versionid = document.getElementById("version");
 	var btntext01 = document.getElementById("btn-text01");
 	var btntext02 = document.getElementById("btn-text02");
-	versionid.innerHTML = vs;
+	versionid.innerHTML = in;
 	
 	var AreInUpdate = false;
 	var NoUnavailable = true;
+	var NoAllUnavailable = true;
 	
 	if ( AreInUpdate === true ) {
-		btntext01.innerText = estxt;
+		btntext01.innerText = on;
 		btntext01.classList.remove("btn-primary")
 		btntext01.classList.add("btn-dark")
 		btntext01.classList.add("blocked")
     }
     if ( NoUnavailable === true ) {
-    	btntext02.innerText = notext;
+    	btntext02.innerText = off;
 		btntext02.classList.remove("btn-primary")
 		btntext02.classList.add("btn-dark")
 		btntext02.classList.add("blocked")
 		}
+	if ( NoAllUnavailable === true ) { 
+		btntext01.innerText = off;
+		btntext01.classList.remove("btn-primary")
+		btntext01.classList.add("btn-dark")
+		btntext01.classList.add("blocked")
+		btntext02.innerText = off;
+		btntext02.classList.remove("btn-primary")
+		btntext02.classList.add("btn-dark")
+		btntext02.classList.add("blocked")
 })
